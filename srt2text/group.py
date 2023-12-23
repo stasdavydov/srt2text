e63c06814a91a5df3e302fc4d43f7ab1ca669329
Stas Davydov <davidovsv@yandex.ru>
2023-12-23 21:10:30 +0700

    Some improvements

--- srt2text/group.py
+++ srt2text/group.py
@@ -1,7 +1,7 @@
 # Extract blocks of text from SRT file with time marks by duration
 
 import click
-from pysrt import SubRipFile
+from pysrt import SubRipFile, SubRipTime
 
 
 def is_eot(text: str):
@@ -12,7 +12,8 @@ def is_eot(text: str):
 @click.argument('srt_file')
 @click.option('--encoding', default='utf8', help='Encoding')
 @click.option('--duration', default=180, type=int, help="Duration in sec")
-def main(srt_file, encoding, duration):
+@click.option('--offset', default=0, type=int, help="Offset in sec")
+def main(srt_file, encoding, duration, offset):
     with open(srt_file.replace('.srt', '.txt'), 'w+', encoding=encoding) as out:
         srt = SubRipFile.open(srt_file, encoding=encoding)
 
@@ -20,12 +21,14 @@ def main(srt_file, encoding, duration):
         prev_text = None
         for item in srt:
             if start == 0:
-                print('%02d:%02d:%02d' % tuple(item.start)[0:3], file=out)
+                print('%02d:%02d:%02d' % tuple(
+                    SubRipTime.from_ordinal(item.start.ordinal + offset*1000)
+                )[0:3], file=out)
             if prev_text != item.text:
                 print(item.text.capitalize() if is_eot(prev_text) else item.text, end=' ', file=out)
             prev_text = item.text
-            start += int(item.duration.ordinal / 1000)
-            if start > duration and is_eot(item.text):
+            start += item.duration.ordinal
+            if int(start/1000) > duration and is_eot(item.text):
                 start = 0
                 print("\n", file=out)
 
